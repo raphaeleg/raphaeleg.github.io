d211e6286d39c78ef2fa4998ab275b7d7eca181b
crushedsummers <raphaeleguillemotmichelle@gmail.com>
2022-07-16 22:18:30 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sorry!
+        </p>
+        <p>
+          I'm still developing this site, if you're interested in what I do, here's my
+          <a href="https://www.linkedin.com/in/raphaele-michelle-guillemot-6a34271a8/">
+            LinkedIn
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
