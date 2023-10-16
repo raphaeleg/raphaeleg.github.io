8690d6c461fb00dec7ff94251ba751188340538d
raphaeleg <quark265@connect.hku.hk>
2023-10-16 09:18:06 +0200

    added ue5, some png to jpg fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,71 +1,76 @@
+/** @format */
 
-import figma from "../assets/tech/figma.png";
-import git from "../assets/tech/git.png";
-import next from "../assets/tech/next.png";
-import unity from "../assets/tech/unity.png";
-import java from "../assets/tech/java.png";
-import firebase from "../assets/tech/firebase.png";
-import illustrator from "../assets/tech/illustrator.png";
-import aftereffects from "../assets/tech/aftereffects.png";
-import rhino from "../assets/tech/rhino.png";
-import cpp from "../assets/tech/c++.png";
-import python from "../assets/tech/python.png";
-import reactjs from "../assets/tech/reactjs.png";
+import figma from '../assets/tech/figma.png';
+import git from '../assets/tech/git.png';
+import next from '../assets/tech/next.png';
+import unity from '../assets/tech/unity.png';
+import java from '../assets/tech/java.png';
+import firebase from '../assets/tech/firebase.png';
+import illustrator from '../assets/tech/illustrator.png';
+import aftereffects from '../assets/tech/aftereffects.png';
+import rhino from '../assets/tech/rhino.png';
+import cpp from '../assets/tech/c++.png';
+import python from '../assets/tech/python.png';
+import reactjs from '../assets/tech/reactjs.png';
+import ue5 from '../assets/tech/ue5.png';
 
 const specialWorks = [
-    {
-        name: "Web development (Next.js & React.js)",
-        icon: next,
-    },
-    {
-        name: "Unity & C#",
-        icon: unity,
-    },
-]
+	{
+		name: 'Web development (Next.js & React.js)',
+		icon: next,
+	},
+	{
+		name: 'Unity & C#',
+		icon: unity,
+	},
+	{
+		name: 'Unreal Engine 5 & C++',
+		icon: ue5,
+	},
+];
 
 const technologies = [
-    // Python, C#, C++, Java, HTML5, CSS3, Javascript, JQuery, Next.js, React Native, Firebase
-    {
-        name: "React Native",
-        icon: reactjs,
-    },
-    {
-        name: "Figma",
-        icon: figma,
-    },
-    {
-        name: "Firebase",
-        icon: firebase,
-    },
-    {
-        name: "Adobe Illustrator",
-        icon: illustrator,
-    },
-    {
-        name: "Adobe After Effects",
-        icon: aftereffects,
-    },
-    {
-        name: "Git",
-        icon: git,
-    },
-    {
-        name: "Rhino",
-        icon: rhino,
-    },
-    {
-        name: "Python",
-        icon: python,
-    },
-    {
-        name: "C++",
-        icon: cpp,
-    },
-    {
-        name: "Java",
-        icon: java,
-    },
-
+	// Python, C#, C++, Java, HTML5, CSS3, Javascript, JQuery, Next.js, React Native, Firebase
+	{
+		name: 'React Native',
+		icon: reactjs,
+	},
+	{
+		name: 'Figma',
+		icon: figma,
+	},
+	{
+		name: 'Firebase',
+		icon: firebase,
+	},
+	{
+		name: 'Adobe Illustrator',
+		icon: illustrator,
+	},
+	{
+		name: 'Adobe After Effects',
+		icon: aftereffects,
+	},
+	{
+		name: 'Git',
+		icon: git,
+	},
+	{
+		name: 'Rhino',
+		icon: rhino,
+	},
+	{
+		name: 'Python',
+		icon: python,
+	},
+	{
+		name: 'C++',
+		icon: cpp,
+	},
+	{
+		name: 'Java',
+		icon: java,
+	},
 ];
 
 export { specialWorks, technologies };
