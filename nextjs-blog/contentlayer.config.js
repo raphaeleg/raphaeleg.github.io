dfe7672f16dc1a0964d01d421c95f39a0131a0b8
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-08-26 09:29:30 +0800

    integrated contentlayer
    for generating blog posts

---
+++ nextjs-blog/contentlayer.config.js
@@ -0,0 +1,45 @@
+import { makeSource, defineDocumentType } from '@contentlayer/source-files'
+
+
+const Blog = defineDocumentType( () => ( {
+    name: 'Blog',
+    filePathPattern: '**/**/*.mdx',
+    contentType: "mdx",
+    fields: {
+        title: {
+            type: 'string',
+            required: true,
+        },
+        publishedAt: {
+            type: 'date',
+            required: true,
+        },
+        desc: {
+            type: 'string',
+            required: true,
+        },
+        image: {
+            type: 'string',
+            required: true,
+        },
+        isPublished: {
+            type: 'boolean',
+            required: true,
+        },
+        tags: {
+            type: 'list',
+            of: { type: 'string' },
+        }
+    },
+    computedFields: {
+        url: {
+            type: 'string',
+            resolve: ( doc ) => `/blogs/${doc._raw.flattenedPath}`,
+        }
+    }
+} ) );
+
+export default makeSource( {
+    contentDirPath: 'content',
+    documentTypes: [Blog],
+} );
--- nextjs-blog/src/app/page.js
+++ nextjs-blog/src/app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { allBlogs } from 'contentlayer/generated'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
+    < main className="flex flex-col items-center justify-center" >
 
-    </main>
+    </main >
   )
 }
