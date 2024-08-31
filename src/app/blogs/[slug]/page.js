4b73934b3c30fd73c30fa174eef470e536a55bd6
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-08-31 11:38:57 +0800

    Adding Metadata for SEO

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -2,6 +2,49 @@ import { allBlogs } from "@/.contentlayer/generated"
 import BlogHeader from "@/src/components/Blog/BlogHeader";
 import RenderMdx from "@/src/components/Blog/RenderMdx";
 import TableOfContents from "@/src/components/Blog/TableOfContents";
+import siteMetadata from "@/src/utils/siteMetaData";
+
+export async function generateMetadata( { params } ) {
+    const blog = allBlogs.find( ( blog ) => blog._raw.flattenedPath === params.slug );
+    if ( !blog ) {
+        return;
+    }
+    const publishedAt = new Date( blog.publishedAt ).toISOString();
+
+    let imageList = [siteMetadata.socialBanner];
+    if ( blog.image ) {
+        imageList =
+            typeof blog.image.filePath === "string"
+                ? [siteMetadata.siteUrl + blog.image.filePath.replace( "../public", "" )]
+                : blog.image;
+    }
+    const ogImages = imageList.map( ( img ) => {
+        return { url: img.includes( "http" ) ? img : siteMetadata.siteUrl + img };
+    } );
+
+    return {
+        title: blog.title,
+        description: blog.description,
+        openGraph: {
+            title: blog.title,
+            description: blog.description,
+            url: siteMetadata.siteUrl + blog.url,
+            siteName: siteMetadata.title,
+            locale: siteMetadata.language,
+            type: "article",
+            publishedTime: publishedAt,
+            modifiedTime: publishedAt,
+            images: ogImages,
+            authors: siteMetadata.title,
+        },
+        twitter: {
+            card: "summary_large_image",
+            title: blog.title,
+            description: blog.description,
+            images: ogImages,
+        },
+    };
+}
 
 export default function BlogPage( { params } ) {
 
