72ba345443fa6725bdcb73d491cdd875a20713d9
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-09-03 16:53:12 +0800

    *** FINALIZED deployment setup
    npm run static && npm run deploy

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     },
     output: "export",
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = withContentlayer( { ...nextConfig } );
